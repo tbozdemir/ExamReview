2352b77dd53118c0e1f1fa0eb7d5c17d829bcc2f
macdougalljs <macdojer@sheridancollege.ca>
2019-05-29 20:47:13 -0400

    initial template setup

---
+++ src/ttj2019/TTJ2019.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ttj2019;
+
+/**
+ *
+ * @author Jeremiah MacDougall, Sheridan College Davis Campus
+ */
+public class TTJ2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
